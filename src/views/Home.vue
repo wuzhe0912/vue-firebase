715417026c20d9da57a701ed862f7f6fed5655ff
Pitt Wu <kgb00128@gmail.com>
2020-02-27 14:42:08 +0800

    mask-map init

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  div.home 123
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
